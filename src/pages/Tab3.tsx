787fb46b1061c5ae7e3551472a365f7f56c73a9e
Juan Esteban Hernández <factoriatech@gmail.com>
2019-10-06 22:51:56 -0400

    contact form(validation pending)

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,15 +1,92 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import React, {useState} from 'react';
+import { 
+  IonHeader, 
+  IonToolbar,
+  IonInput, 
+  IonPage, 
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonTextarea,
+  IonToast,
+  IonButton
+} from '@ionic/react';
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const SAVE_CONTACT_ENTRY = gql`
+  mutation insert_entry($objects: [contact_insert_input!]!) {
+        insert_contact(objects: $objects) {
+        returning {
+        id
+        }
+    }
+  }
+`;
 
 const Tab3Page: React.FC = () => {
+  const [insert_contact] = useMutation(SAVE_CONTACT_ENTRY);
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [asunto,setAsunto] = useState('');
+  const [mensaje,setMensaje] = useState('');
+  const [alertar,setAlertar] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const handleForm = (e:any) => {
+    e.preventDefault();
+    insert_contact({variables: {objects:{
+      name,
+      subject: asunto,
+      email,
+      message: mensaje
+    }}}).then((x:any) => {
+      if(x){
+        setShowToast(true)
+      }
+      setName('');
+      setEmail('');
+      setAsunto('');
+      setMensaje('')
+    });
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab Three</IonTitle>
+          <IonTitle>Contactanos</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent />
+      <IonContent>
+        <p>Para cualquier inquitued que necesites, puedes contactarnos con el formulario mas abajo</p>
+        <form onSubmit={handleForm} noValidate={true}>
+            <IonItem>
+          <IonLabel position="floating">Nombre</IonLabel>
+          <IonInput value={name} onIonChange={(e:any) => setName(e.target.value)} required={true}></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput value={email} onIonChange={(e:any) => setEmail(e.target.value)} required></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Asunto</IonLabel>
+            <IonInput value={asunto} onIonChange={(e:any) => setAsunto(e.target.value)} required></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonTextarea placeholder="Mensaje..." value={mensaje} onIonChange={(e:any) => setMensaje(e.target.value)} required></IonTextarea>
+          </IonItem>
+          <IonButton type="submit" color="primary" expand="block" >Enviar</IonButton>
+        </form>
+        <IonLabel color="danger">{alertar}</IonLabel>
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message="Su mensaje fue enviado!"
+          duration={2000}
+        />
+  </IonContent>
     </IonPage>
   );
 };
